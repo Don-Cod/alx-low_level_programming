633df2d121f87d3a5b43fb43ddbabc11b47ce8ad
Samuel Meyo <masyome@gmail.com>
2022-12-19 20:02:52 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,11 +3,9 @@
 /**
  * jack_bauer - Prints every minute of the day of
  * Jack Bauer, starting from 00:00 to 23:59.
- *
  **/
 
 void jack_bauer(void)
-
 {
 	int hour, minute;
 
@@ -15,10 +13,12 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar((hour / 10) + '0');							_putchar((hour % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
-			_putchar('\n'); 							}
-											}
+			_putchar('\n');
+		}
+	}
 }
