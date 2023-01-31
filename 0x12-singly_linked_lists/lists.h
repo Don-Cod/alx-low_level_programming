046445a4c27ce0c4054e817d6e4dec77a1fccd92
Samuel Meyo <masyome@gmail.com>
2023-01-30 18:32:11 -0800

    0x12

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,22 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
 /**
- * struct lists_s - singly linked lists
+ * struct lists_s - singly linked list
  * @str: string - (malloc'ed sting)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: signly linked list node structure
- */
+ * Description: singly linked list node structure
+ **/
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} liata;
+} list_t;
 
-size_t prints_list(const list_t *h);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head,const char *str);
