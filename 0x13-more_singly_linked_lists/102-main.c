51fbc68c2baa10e2eca63833649fadd95113445b
Samuel Meyo <masyome@gmail.com>
2023-02-01 21:35:08 -0800

    102-main.c fix

--- 0x13-more_singly_linked_lists/102-main.c
+++ 0x13-more_singly_linked_lists/102-main.c
@@ -15,16 +15,6 @@ int main(void)
 	listint_t *head2;
 	listint_t *node;
 
-	head2 = NULL;
-	add_nodeint(&head2, 0);
-	add_nodeint(&head2, 1);
-	add_nodeint(&head2, 2);
-	add_nodeint(&head2, 3);
-	add_nodeint(&head2, 4);
-	add_nodeint(&head2, 98);
-	add_nodeint(&head2, 402);
-	add_nodeint(&head2, 1024);
-	print_listint_safe(head2);
 	head = NULL;
 	node = add_nodeint(&head, 0);
 	add_nodeint(&head, 1);
@@ -35,6 +25,16 @@ int main(void)
 	add_nodeint(&head, 402);
 	add_nodeint(&head, 1024);
 	print_listint_safe(head);
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
+	add_nodeint(&head2, 98);
+	add_nodeint(&head2, 402);
+	add_nodeint(&head2, 1024);
+	print_listint_safe(head2);
 	printf("%p, %p\n", (void *)head2, (void *)head);
 	return (0);
 }
