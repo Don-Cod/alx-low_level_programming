4833eb050464f2f3fb2901bfb2d737bb2ca8f286
Samuel Meyo <masyome@gmail.com>
2023-06-28 14:49:24 -0700

    fix

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-largest_palindrome = 0
+def ispalindrome(n):
+    string = str(n)
+    return string == string[::-1]
 
+array = []
 for i in range(999, 99, -1):
     for j in range(i, 99, -1):
-        product = i * j
-        if str(product) == str(product)[::-1] and product > largest_palindrome:
-            largest_palindrome = product
-
-# Save the result in the file
-with open("102-result", "w") as file:
-    file.write(str(largest_palindrome))
-
+        if ispalindrome(i * j):
+            array.append(i * j)
+print(max(array))
